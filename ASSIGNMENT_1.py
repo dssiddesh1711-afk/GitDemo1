94ce504adfc92d4eef4dfde3517a4bfe66c1f630
dssiddesh1711-afk <dssiddesh1711@gmail.com>
2026-01-19 12:51:26 +0530

    Assignment_1 added success

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,36 @@
+
+
+"""Task 1: Perform Basic Mathematical Operations"""
+
+x=int(input("Enter the first number: "))
+"""Takes input from user"""
+y=int(input("Enter the second number: "))
+"""Takes input from user"""
+
+Addition=x+y
+"""Addition formula"""
+Substraction = x-y
+"""Substraction formula"""
+Multiplication = x*y
+"""Multiplication formula"""
+Division = (x/y)
+"""Division formula"""
+
+print("Addition :",Addition)
+print("Substraction :",Substraction)
+print("Multiplication :",Multiplication)
+print("Division:",Division)
+
+"Task 2: Create a Personalized Greeting"
+
+
+a=input("Enter your First Name: ")
+"""Taking input from User fro First Name"""
+b=input("enter your Second Name:")
+"""Taking input from User From Second Name"""
+c=a+b
+"""String Contactination"""
+
+print("Hello,", c,"!" ,"Welcome to the Python Program." )
+"""Printing with pre-defined other messsages"""
+
